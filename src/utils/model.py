d6ecf7ebbca52e4d230b2275188b4f51981ec65d
Kamal Kant <kkchaudhary11@gmail.com>
2021-10-10 16:34:01 +0530

    added the model save method

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,4 +1,8 @@
+import time
+
 import tensorflow as tf
+import os
+
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     LAYERS = [
@@ -12,7 +16,17 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
 
     model_clf.summary()
 
-
     model_clf.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER, metrics=METRICS)
 
     return model_clf
+
+
+def get_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+
+def save_model(model, model_name, model_dir):
+    unique_file_name = get_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_file_name)
+    model.save(path_to_model)
